5c074a8ee7e85d1a3cf2df837e077934bf1c1897
zerozane5758 <152180313+zerozane5758@users.noreply.github.com>
2024-11-26 20:46:50 +0700

    Initial commit

---
+++ app/src/main/java/com/c14220203/cobafirebase/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.c14220203.cobafirebase
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import java.util.HashMap
+
+class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+    var DataProvinsi = ArrayList<daftarProvinsi>()
+    lateinit var lvAdapter: SimpleAdapter
+
+    lateinit var _etProvinsi: EditText
+    lateinit var _etIbukota: EditText
+
+    var data: MutableList<Map<String, String>> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        _etProvinsi = findViewById<EditText>(R.id.etProvinsi)
+        _etIbukota = findViewById<EditText>(R.id.etIbukota)
+        val _btnSimpan = findViewById<Button>(R.id.btSimpan)
+        val _lvData = findViewById<ListView>(R.id.lvData)
+
+        lvAdapter = SimpleAdapter(
+            this,
+            data,
+            android.R.layout.simple_list_item_2,
+            arrayOf<String>("Pro", "Ibu"),
+            intArrayOf(
+                android.R.id.text1,
+                android.R.id.text2
+            )
+        )
+        _lvData.adapter = lvAdapter
+
+        _btnSimpan.setOnClickListener {
+            TambahData(db, _etProvinsi.text.toString(), _etIbukota.text.toString())
+        }
+
+        readData(db)
+    }
+
+
+    fun TambahData(db :FirebaseFirestore, Provinsi: String, Ibukota: String){
+        val dataBaru = daftarProvinsi(Provinsi, Ibukota)
+        db.collection("tbProvinsi")
+            .add(dataBaru)
+            .addOnSuccessListener {
+                _etProvinsi.setText("")
+                _etIbukota.setText("")
+                readData(db)
+                Log.d("Firebase", "Data Berhasil Disimpan")
+            }
+            .addOnFailureListener {
+                Log.d("Firebase", it.message.toString())
+            }
+    }
+
+    fun readData(db: FirebaseFirestore) {
+        db.collection("tbProvinsi").get()
+            .addOnSuccessListener {
+                result ->
+                DataProvinsi.clear()
+                for (document in result) {
+                    val readData = daftarProvinsi(
+                        document.data.get("provinsi").toString(),
+                        document.data.get("ibukota").toString()
+                    )
+                    DataProvinsi.add(readData)
+                    data.clear()
+                    DataProvinsi.forEach {
+                       val dt: MutableMap<String, String> = HashMap(2)
+                        dt["Pro"] = it.provinsi
+                        dt["Ibu"] = it.ibukota
+                        data.add(dt)
+                    }
+                }
+                lvAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener {
+                Log.d("Firebase", it.message.toString())
+            }
+    }
+}
---
+++ app/src/main/java/com/c14220203/cobafirebase/daftarProvinsi.kt
@@ -0,0 +1,6 @@
+package com.c14220203.cobafirebase
+
+data class daftarProvinsi(
+    var provinsi: String,
+    var ibukota: String
+)
